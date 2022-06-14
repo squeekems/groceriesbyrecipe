fd7faf362457cf90a14c26dacd444ee8e6a99ba7
Jack Kern <jackrkern@gmail.com>
2022-06-14 17:01:33 -0400

    I did some shit

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,27 +1,24 @@
 document.addEventListener("DOMContentLoaded", () =>
 {
-  document.querySelectorAll(".search-input").forEach(inputField => 
+  const inputField = document.getElementById("search-input");
+  const tblItems = document.getElementById("tblItems");
+  const tableRows = tblItems.querySelectorAll("tbody tr");
+  inputField?.addEventListener("input", () =>
+  {
+    const searchQuery = inputField.value.toLowerCase();
+    console.log(searchQuery);
+    for (const tableCell of (tableRows))
     {
-      const tableRows = inputField.closest("table").querySelectorAll("tbody tr");
-      const headerCell = inputField.closest("th");
-      const otherHeaderCells = inputField.closest("tr").querySelectorAll("th");
-      const columnIndex = Array.from(otherHeaderCells).indexOf(headerCell);
-      const searchableCells = Array.from(tableRows).map(row => row.querySelectorAll("td")[columnIndex]);
-      inputField.addEventListener("input", () =>
+      const row = tableCell.closest("tr");
+      const value = tableCell.textContent.toLowerCase().replace(",","");
+      row.style.visibility = null;
+      if (value.search(searchQuery) === -1)
       {
-        const searchQuery = inputField.value.toLowerCase();
-        for (const tableCell of searchableCells)
-        {
-          const row = tableCell.closest("tr");
-          const value = tableCell.textContent.toLowerCase().replace(",","");
-          row.style.visibility = null;
-          if (value.search(searchQuery) === -1)
-          {
-            row.style.visibility = "collapse";
-          }
-        }
-      });
-    });
+        row.style.visibility = "collapse";
+      }
+      console.log(inputField.value.toLowerCase());
+    }
+  });
 });
 $('#lstItemsInStore').on('click', '.clickable-row', function(event) {
   $(this).toggleClass('bg-info').siblings().removeClass('bg-info');
@@ -32,4 +29,5 @@ $('#addItemModal').on('show.bs.modal', function (event) {
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
-})
+});
+console.log("This is running");
