2eb299c9e42152eea3de6b85261a5251dec52ae1
Jack Kern <jackrkern@gmail.com>
2022-06-14 22:38:49 -0400

    Button functionality

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,37 @@
+$(function()
+{
+  let getSelectedRow = function()
+  {
+    return $('tr.bg-info');
+  }
+
+  $('#tblItems').on('click', '.clickable-row', function(event)
+  {
+    $(this).toggleClass('bg-info').siblings().removeClass('bg-info');
+
+    let rows = getSelectedRow();
+    console.log(rows);
+    console.log(rows.prop("classList"));
+    console.log(rows != undefined);
+    console.log(rows != null);
+    if (rows.prop("classList") != undefined)
+    {
+      $('.item-buttons').removeClass('disabled');
+    }else
+    {
+      $('.item-buttons').addClass('disabled');
+    }
+  });
+
+  $('#addItemModal').on('show.bs.modal', function (event) {
+    const button = $(event.relatedTarget) // Button that triggered the modal
+    const recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    const modal = $(this)
+  });
+});
+
 document.addEventListener("DOMContentLoaded", () =>
 {
   const inputField = document.getElementById("search-input");
@@ -6,7 +40,6 @@ document.addEventListener("DOMContentLoaded", () =>
   inputField?.addEventListener("input", () =>
   {
     const searchQuery = inputField.value.toLowerCase();
-    console.log(searchQuery);
     for (const tableCell of (tableRows))
     {
       const row = tableCell.closest("tr");
@@ -16,18 +49,70 @@ document.addEventListener("DOMContentLoaded", () =>
       {
         row.style.visibility = "collapse";
       }
-      console.log(inputField.value.toLowerCase());
     }
   });
 });
-$('#lstItemsInStore').on('click', '.clickable-row', function(event) {
-  $(this).toggleClass('bg-info').siblings().removeClass('bg-info');
-});
-$('#addItemModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-});
-console.log("This is running");
+
+function sortTable(n)
+{
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("tblItems");
+  switching = true;
+
+  // Set the sorting direction to ascending:
+  dir = "asc"; 
+
+  // Make a loop that will continue until no switching has been done:
+  while (switching)
+  {
+    // start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+
+    // Loop through all table rows (except the first, which contains table headers):
+    for (i = 0; i < (rows.length - 1); i++)
+    {
+      // start by saying there should be no switching:
+      shouldSwitch = false;
+
+      // Get the two elements you want to compare, one from current row and one from the next:
+      x = rows[i].getElementsByTagName("td")[n];
+      y = rows[i + 1].getElementsByTagName("td")[n];
+
+      // check if the two rows should switch place, based on the direction, asc or desc:
+      if (dir == "asc")
+      {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase())
+        {
+          // if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase())
+        {
+          // if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch)
+    {
+      // If a switch has been marked, make the switch and mark that a switch has been done:
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else
+    {
+      // If no switching has been done AND the direction is "asc", set the direction to "desc" and run the while loop again.
+      if (switchcount == 0 && dir == "asc")
+      {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
